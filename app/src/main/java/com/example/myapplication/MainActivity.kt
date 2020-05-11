e4ec1e7424b943bf44db39eb76ddc5cd6214c193
Sergey Savenko <sa.savenko2@gmail.com>
2020-05-11 18:52:20 +0700

    Add Visualizer for vibration

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,9 +4,16 @@ package com.example.myapplication
 //import android.R.layout
 import android.Manifest
 import android.app.Activity
-import android.content.*
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
+import android.media.AudioManager
 import android.media.MediaPlayer
+import android.media.audiofx.Equalizer
+import android.media.audiofx.Visualizer
+import android.media.audiofx.Visualizer.MeasurementPeakRms
 import android.os.*
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
@@ -19,10 +26,117 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.myapplication.MusicWatcher.WatcherBinder
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Timer
+import kotlin.concurrent.schedule
 
-class MainActivity : AppCompatActivity(), ServiceConnection {
+
+class MainActivity : AppCompatActivity(), ServiceConnection, Visualizer.OnDataCaptureListener {
 
     private var mediaPlayer : MediaPlayer? = null
+    private var visualiser : Visualizer? = null
+    private var mEqualizer : Equalizer? = null
+    private var mTimer : Timer ? = null
+
+    private val CAPTURE_SIZE = 256
+
+    private var type = 1
+
+    override fun onWaveFormDataCapture(visualizer: Visualizer?, waveform: ByteArray?, samplingRate: Int) {
+
+        val vibrator =
+            getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        val notNullable : ByteArray = waveform ?: ByteArray(0)
+
+        val shiftedWaveform = notNullable.map({it.toUByte().toInt() - 128})
+
+        val waveData = shiftedWaveform
+
+        val max = waveData.max() ?: 0
+        val min = waveData.min() ?: 0
+        val sqrSum = waveData.fold(0) {sum, elem -> sum + elem*elem}
+        val rms = kotlin.math.sqrt(sqrSum.toDouble())
+
+        val toDBm = { value: Double -> 20 * kotlin.math.log10(kotlin.math.abs(value)) }
+        val normalize = { value: Double, maxValue: Double -> -9600 * value / maxValue }
+
+        val rmsDBm = toDBm(rms)
+        val rmsLin = normalize(rms, 128.0 * 128)
+
+        val maxDBm = toDBm(max.toDouble())
+        val minDBm = toDBm(min.toDouble())
+
+        val maxLin = normalize(max.toDouble(), 128.0)
+        val minLin = normalize(min.toDouble(), -128.0)
+
+        val r = { value : Double -> kotlin.math.round(value)}
+
+        val message = """
+    Original  Rms ${r(rms)} Min $min Max $max
+    """
+        Log.i("", message)
+
+        var measurement = MeasurementPeakRms()
+        if(visualizer?.enabled ?: false) {
+            visualizer?.getMeasurementPeakRms(measurement)
+        }
+        Log.i("", "peak : " + measurement.mPeak + " rms : " + measurement.mRms)
+
+//        var measurement = MeasurementPeakRms()
+//        if(visualizer?.enabled ?: false) {
+//            visualizer?.getMeasurementPeakRms(measurement)
+//        }
+//        Log.i(ContentValues.TAG, "peak : " + measurement.mPeak + " rms : " + measurement.mRms)
+//        if (measurement.mPeak > -4500) vibrator.vibrate(15)
+ //       if(measurement.mPeak > 3500 || measurement.mPeak < -3500) vibrator.vibrate(20)
+//        if (avg > 20.0 || avg < -20.0) {
+//            vibrator.vibrate(15)
+//        }
+
+//        Log.i(ContentValues.TAG, avg.toString())
+
+        // TEST
+        if(
+            (measurement.mPeak > -6000 && type == 0) || // 6000 это почти мин
+            (measurement.mRms > -4500 && type == 1) || // 6000 хорошо себя на середине показывает
+            (max > 20 && type == 2)|| // в идеале надо 64, но так не работает //дергано звучит
+            (rms > 150 && type == 3) // 127 * sqr(256) / 2 // в идеале бы 1016 но так не работает
+        ) {
+            vibrator.vibrate(30)
+        }
+    }
+
+    override fun onFftDataCapture(visualizer: Visualizer?, fft: ByteArray?, samplingRate: Int) {}
+
+    private fun startVisualiser() {
+
+//        val am = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+//        val originalVolume = am.getStreamVolume(STREAM_MUSIC)
+//        am.setStreamVolume(
+//            STREAM_MUSIC,
+//            am.getStreamMaxVolume(STREAM_MUSIC), 0
+//        )
+
+        if(visualiser != null) {
+            visualiser?.setEnabled(true)
+            return
+        }
+
+        visualiser = Visualizer(mediaPlayer?.getAudioSessionId() ?: 0)
+        visualiser?.setDataCaptureListener(this, Visualizer.getMaxCaptureRate(), true, false)
+
+        // Ugly hack https://github.com/felixpalmer/android-visualizer/issues/19
+        mEqualizer = Equalizer(0, mediaPlayer?.getAudioSessionId() ?: 0)
+        mEqualizer?.setEnabled(true)
+
+ //       val t = Visualizer.getMaxCaptureRate()
+ //       val t2 = Visualizer.getCaptureSizeRange()
+
+        visualiser?.setCaptureSize(CAPTURE_SIZE)
+        visualiser?.setEnabled(true)
+        visualiser?.setMeasurementMode(Visualizer.MEASUREMENT_MODE_PEAK_RMS)
+        visualiser?.setScalingMode(Visualizer.SCALING_MODE_NORMALIZED)
+    }
 
 //    @SuppressLint("HandlerLeak")
     var handler: Handler = object : Handler() {
@@ -31,14 +145,20 @@ class MainActivity : AppCompatActivity(), ServiceConnection {
                 val vibrator =
                     getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 //                val t = vibrator.hasAmplitudeControl()
-                Log.i(ContentValues.TAG, (msg.obj as Int).toString())
+//                Log.i(ContentValues.TAG, (msg.obj as Int).toString())
 
                 val volume = msg.obj as Int
                 if (volume > 225) {
-                    vibrator.vibrate(15)
+//                    vibrator.vibrate(15)
                 }
-//                vibrator.vibrate(longArrayOf(20), -1)
-//                vibrator.vibrate(createWaveform(longArrayOf(20), intArrayOf(msg.obj as Int), -1) )
+
+//                var measurement = MeasurementPeakRms()
+//                if(visualiser?.enabled ?: false) {
+//                    visualiser?.getMeasurementPeakRms(measurement)
+//                    Log.i(ContentValues.TAG, "peak : " + measurement.mPeak + " rms : " + measurement.mRms)
+// //                   if (measurement.mPeak > -4500) vibrator.vibrate(15)
+//                      if (measurement.mRms > -5000) vibrator.vibrate(15)
+//                }
             }
         }
     }
@@ -46,6 +166,8 @@ class MainActivity : AppCompatActivity(), ServiceConnection {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setVolumeControlStream (AudioManager.STREAM_MUSIC)
 
         //BUTTON CLICK
         choose_img_button.setOnClickListener {
@@ -93,17 +215,18 @@ class MainActivity : AppCompatActivity(), ServiceConnection {
         image_view.setOnTouchListener { _, event ->
             if(event.getAction() == ACTION_MOVE) {
                 playAudio()
+                startVisualiser()
 //                vibrate()
             }
             true
         }
 
         requestPermission()
-        bindService(
-            Intent(this, MusicWatcher::class.java),
-            this,
-            Context.BIND_AUTO_CREATE
-        )
+//        bindService(
+//            Intent(this, MusicWatcher::class.java),
+//            this,
+//            Context.BIND_AUTO_CREATE
+//        )
     }
 
     override fun onServiceConnected(name: ComponentName?, service: IBinder) {
@@ -119,11 +242,18 @@ class MainActivity : AppCompatActivity(), ServiceConnection {
     override fun onServiceDisconnected(name: ComponentName?) {}
 
     private fun playAudio() {
-        if(mediaPlayer?.isPlaying() ?: false) {
-            mediaPlayer?.pause()
-            mediaPlayer?.seekTo(0)
-        }
+//        if(mediaPlayer?.isPlaying() ?: false) {
+//            mediaPlayer?.pause()`
+//            mediaPlayer?.seekTo(0)
+//        }
         mediaPlayer?.start()
+
+        mTimer?.cancel()
+        mTimer?.purge()
+        mTimer = Timer("SettingUp", false)
+        mTimer?.schedule(3000) {
+            mediaPlayer?.pause()
+        }
     }
 
     private fun vibrate() {
@@ -210,6 +340,11 @@ class MainActivity : AppCompatActivity(), ServiceConnection {
         if (resultCode == Activity.RESULT_OK && requestCode == SOUND_PICK_CODE){
 //            mediaPlayer = MediaPlayer.create(this, R.raw.beep)
             mediaPlayer = MediaPlayer.create(this, data?.data)
+            mediaPlayer?.setLooping(true)
+            visualiser = null // invalidate
+            mediaPlayer?.setOnCompletionListener {
+                visualiser?.setEnabled(false)
+            }
         }
     }
 }
