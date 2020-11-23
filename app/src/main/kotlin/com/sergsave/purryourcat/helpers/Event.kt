3fde1cef5041534073c0297c4e95fe5e20f0449f
Sergey Savenko <sa.savenko2@gmail.com>
2020-11-23 21:06:57 +0700

    Fill about screen

--- app/src/main/kotlin/com/sergsave/purryourcat/helpers/Event.kt
+++ app/src/main/kotlin/com/sergsave/purryourcat/helpers/Event.kt
@@ -1,28 +1,15 @@
-/*
- * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.sergsave.purryourcat.helpers
 
 import androidx.lifecycle.Observer
+
+// https://gist.github.com/JoseAlcerreca/5b661f1800e1e654f07cc54fe87441af#file-event-kt
+// https://gist.github.com/JoseAlcerreca/e0bba240d9b3cffa258777f12e5c0ae9#file-eventobserver-kt
 
 /**
  * Used as a wrapper for data that is exposed via a LiveData that represents an event.
  */
 open class Event<out T>(private val content: T) {
 
-    @Suppress("MemberVisibilityCanBePrivate")
     var hasBeenHandled = false
         private set // Allow external read but not write
 
@@ -52,8 +39,8 @@ open class Event<out T>(private val content: T) {
  */
 class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
     override fun onChanged(event: Event<T>?) {
-        event?.getContentIfNotHandled()?.let {
-            onEventUnhandledContent(it)
+        event?.getContentIfNotHandled()?.let { value ->
+            onEventUnhandledContent(value)
         }
     }
 }
---
+++ app/src/main/kotlin/com/sergsave/purryourcat/helpers/OpenRateAppLink.kt
@@ -0,0 +1,31 @@
+package com.sergsave.purryourcat.helpers
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+
+fun AppCompatActivity.openRateAppLink() {
+    try {
+        startActivity(makeRateIntent(this, "market://details"))
+    } catch (e: ActivityNotFoundException) {
+        startActivity(makeRateIntent(this, "https://play.google.com/store/apps/details"))
+    }
+}
+
+private fun makeRateIntent(context: Context, baseUrl: String): Intent {
+    val url = Uri.parse("$baseUrl?id=${context.packageName}")
+
+    val extraFlag = if (Build.VERSION.SDK_INT >= 21)
+        Intent.FLAG_ACTIVITY_NEW_DOCUMENT
+    else {
+        @Suppress("DEPRECATION")
+        Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET
+    }
+
+    return Intent(Intent.ACTION_VIEW, url).apply {
+        addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_MULTIPLE_TASK or extraFlag)
+    }
+}
